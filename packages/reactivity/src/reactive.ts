eb90010751703995dca0c0818cd5694e20be82f1
hefeng6500 <2443992009@qq.com>
2021-06-29 20:57:33 +0800

    fix: reactive add cache

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,4 +1,4 @@
-import { isObject,  } from "@vue/shared";
+import { isObject } from "@vue/shared";
 import {
   mutableHandlers,
   shallowReactiveHandlers,
@@ -22,11 +22,24 @@ export function shallowReadonly(target) {
   return createReactiveObject(target, false, shallowReadonlyHandlers);
 }
 
+const readonlyMap = new WeakMap();
+const reactiveMap = new WeakMap();
+
 export function createReactiveObject(target, isReadonly, baseHandlers) {
   if (!isObject(target)) {
     return target;
   }
 
+  const proxyMap = isReadonly ? readonlyMap : reactiveMap;
+  const existProxy = proxyMap.get(target);
+debugger
+  if (existProxy) {
+    return existProxy;
+  }
+
   const proxy = new Proxy(target, baseHandlers);
+
+  proxyMap.set(target, proxy);
+
   return proxy;
 }
