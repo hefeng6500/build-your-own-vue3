0c085f1d32468f02c670e6a3be7633ccfbee694d
hefeng6500 <2443992009@qq.com>
2022-03-13 10:56:10 +0800

    workflow: use esbuild for dev scripts

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,28 +1,62 @@
-const fs = require("fs");
-const execa = require("execa");
-const { fuzzyMatchTarget } = require("./utils");
+// esbuild 在 dev 环境更快
 
+const { build } = require("esbuild");
+const { resolve, relative } = require("path");
+// 格式化命令行参数
 const args = require("minimist")(process.argv.slice(2));
-const target = args._.length ? fuzzyMatchTarget(args._)[0] : "reactivity";
-const formats = args.formats || args.f;
-const sourceMap = args.sourcemap || args.s;
-// const commit = execa.sync('git', ['rev-parse', 'HEAD']).stdout.slice(0, 7)
+// 控制台高亮插件
+const chalk = require("chalk");
 
-execa(
-  "rollup",
-  [
-    "-wc",
-    "--environment",
-    [
-      // `COMMIT:${commit}`,
-      `TARGET:${target}`,
-      `FORMATS:${formats || "global"}`,
-      sourceMap ? `SOURCE_MAP:true` : ``,
-    ]
-      .filter(Boolean)
-      .join(","),
-  ],
-  {
-    stdio: "inherit",
-  }
+// 打包的目标包名
+const target = args._[0] || "vue";
+// 打包的格式，默认使用 global
+const format = args.f || "global";
+// 目标包的 package.json
+const pkg = require(resolve(__dirname, `../packages/${target}/package.json`));
+
+// 打包输出的格式
+const outputFormat = format.startsWith("global")
+  ? "iife"
+  : format === "cjs"
+  ? "cjs"
+  : "esm";
+
+// 打包输出包名的后缀
+const postfix = format.endsWith("-runtime")
+  ? `runtime.${format.replace(/-runtime$/, "")}`
+  : format;
+
+// 打包输出的文件路径
+const outfile = resolve(
+  __dirname,
+  `../packages/${target}/dist/${target}.${postfix}.js`
 );
+// 获取输出文件的相对路径
+const relativeOutfile = relative(process.cwd(), outfile);
+
+// 使用 esbuild 进行打包
+// 为什么 pnpm build 使用 rollup，而 pnpm dev 使用 esbuild？
+// 因为 esbuild 更快。rollup 打包产物更小
+
+build({
+  entryPoints: [resolve(__dirname, `../packages/${target}/src/index.ts`)],
+  outfile,
+  bundle: true,
+
+  sourcemap: true,
+  format: outputFormat,
+  globalName: pkg.buildOptions?.name,
+  platform: format === "cjs" ? "node" : "browser",
+
+  define: {
+    __VERSION__: `"${pkg.version}"`,
+    __DEV__: `true`,
+  },
+  watch: {
+    onRebuild(error) {
+      if (!error) console.log(`rebuilt: ${relativeOutfile}`);
+    },
+  },
+}).then(() => {
+  console.log(chalk.green(`watching: ${relativeOutfile}`));
+});
