10a8277b3313615c7ab9035840cf3c27af265022
hefeng6500 <2443992009@qq.com>
2022-03-13 16:20:53 +0800

    test: add effect test

---
+++ packages/reactivity/__tests__/effect.spec.ts
@@ -0,0 +1,36 @@
+import { reactive, effect } from "../src/index";
+
+describe("reactivity/effect", () => {
+  it("should allow nested effects", () => {
+    const nums = reactive({ num1: 0, num2: 1, num3: 2 });
+    const dummy: any = {};
+
+    const childSpy = jest.fn(() => (dummy.num1 = nums.num1));
+    const childeffect = effect(childSpy);
+    const parentSpy = jest.fn(() => {
+      dummy.num2 = nums.num2;
+      childeffect();
+      dummy.num3 = nums.num3;
+    });
+    effect(parentSpy);
+
+    expect(dummy).toEqual({ num1: 0, num2: 1, num3: 2 });
+    expect(parentSpy).toHaveBeenCalledTimes(1);
+    expect(childSpy).toHaveBeenCalledTimes(2);
+    // this should only call the childeffect
+    nums.num1 = 4;
+    expect(dummy).toEqual({ num1: 4, num2: 1, num3: 2 });
+    expect(parentSpy).toHaveBeenCalledTimes(1);
+    expect(childSpy).toHaveBeenCalledTimes(3);
+    // this calls the parenteffect, which calls the childeffect once
+    nums.num2 = 10;
+    expect(dummy).toEqual({ num1: 4, num2: 10, num3: 2 });
+    expect(parentSpy).toHaveBeenCalledTimes(2);
+    expect(childSpy).toHaveBeenCalledTimes(4);
+    // this calls the parenteffect, which calls the childeffect once
+    nums.num3 = 7;
+    expect(dummy).toEqual({ num1: 4, num2: 10, num3: 7 });
+    expect(parentSpy).toHaveBeenCalledTimes(3);
+    expect(childSpy).toHaveBeenCalledTimes(5);
+  });
+});
